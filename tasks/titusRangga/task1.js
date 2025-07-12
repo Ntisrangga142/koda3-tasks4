3235be8d5aef21a54499acc4f39ebbcd57e511dd
Titus Rangga <titusrangga142@gmail.com>
2025-07-12 16:19:38 +0700

    feat: add function to make triangle with format like task1

---
+++ tasks/titusRangga/task1.js
@@ -0,0 +1,20 @@
+function isValidPositiveNumber(input) {
+  return typeof input === 'number' &&  input > 0;
+}
+
+function triangle (n){
+    for(let i = 1; i <= n; i++){
+        let kalimat = ""
+        for(let j = 1; j <= i; j++){
+            kalimat += `${j} `
+        }
+        console.log(kalimat)
+    }
+}
+
+exports.main1 = function (number){
+    if(!isValidPositiveNumber(number)) return new Error ('Parameter harus berupa tipe data nomor dan harus positif !');
+
+    triangle(number);
+}
+
