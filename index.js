50b9facdc7180395f07b94372b7ebc3aea9bd94c
Titus Rangga <titusrangga142@gmail.com>
2025-07-12 23:34:23 +0700

    fix: modify error handling for task1.main1, task3.main2, and task5.main3

--- index.js
+++ index.js
@@ -57,13 +57,10 @@ const task4 = require("./tasks/titusRangga/task4.js");
 const task5 = require("./tasks/titusRangga/task5.js");
 const task6 = require("./tasks/titusRangga/task6.js");
 
-const number = 10;
-try{
-    task1.main1(number)
-    console.log()
-}catch (err){
-    console.log(err.message);
-}
+const number = 0;
+
+task1.main1(number);
+console.log()
 
 let countArray_1 = [1, 2, 3, 4, 5]
 let countArray_2 = [2, 2, 2, 2, 2]
@@ -72,23 +69,15 @@ console.log(task2.operationTimes1(countArray_1))
 console.log(task2.operationTimes1(countArray_2))
 console.log()
 
-try{
-    task3.main2(number)
-    console.log()
-}catch (err){
-    console.log(err.message);
-}
+task3.main2(number);
+console.log()
 
 console.log(task4.operationTimes2(countArray_1))
 console.log(task4.operationTimes2(countArray_2))
 console.log()
 
-try{
-    task5.main3(number)
-    console.log()
-}catch (err){
-    console.log(err.message);
-}
+task5.main3(number);
+console.log()
 
 console.log(task6.buatRentang(1, 5))
 console.log(task6.buatRentang(5, 10))
