4ecf0bb424baded0a17d68a62e79321e86815bd2
Titus Rangga <titusrangga142@gmail.com>
2025-07-12 16:33:27 +0700

    fix: modify index.js to access all functions in the titusRangga folder

--- index.js
+++ index.js
@@ -1,51 +1,42 @@
-/**
- * Task Irul
- */
+const task1 = require("./tasks/titusRangga/task1.js");
+const task2 = require("./tasks/titusRangga/task2.js");
+const task3 = require("./tasks/titusRangga/task3.js");
+const task4 = require("./tasks/titusRangga/task4.js");
+const task5 = require("./tasks/titusRangga/task5.js");
+const task6 = require("./tasks/titusRangga/task6.js");
 
-const irul = require("./tasks/irul");
-
-function tasksIrul(){
-    // Program: Sum Number
-    // Success
-    const resultSum = irul.sumNumber(10,50);
-    console.log(resultSum);
-
-    // Failed
-    try{
-        const result = irul.sumNumber("10",30);
-        console.log(result);
-    }catch(error){
-        console.log(error.message);
-    }
-    // Program: Multiply Number
-    // Success
-    const resultMultiply = irul.multiplyNumbers(10,50);
-    console.log(resultMultiply);
-
-    // Failed
-    try{
-        const result = irul.multiplyNumbers("10",30);
-        console.log(result);
-    }catch(error){
-        console.log(error.message);
-    }
-    // Program: Convert Celcius to Fahrenheit
-    // Success
-    const temperatureResult = irul.celciusToFahrenheit(10);
-    console.log(`the temperature ${temperatureResult.celcius}°C is ${temperatureResult.fahrenheit}°F`);
-
-    // Failed
-    try{
-        const result = irul.celciusToFahrenheit("10",30);
-        console.log(result);
-    }catch(error){
-        console.log(error.message);
-    }
+const number = 10;
+try{
+    task1.main1(number)
+    console.log()
+}catch (err){
+    console.log(err.message);
 }
-tasksIrul();
 
-/**
- * Task "Person Name"
- */
+let countArray_1 = [1, 2, 3, 4, 5]
+let countArray_2 = [2, 2, 2, 2, 2]
 
-// import your tasks here
+console.log(task2.operationTimes1(countArray_1))
+console.log(task2.operationTimes1(countArray_2))
+console.log()
+
+try{
+    task3.main2(number)
+    console.log()
+}catch (err){
+    console.log(err.message);
+}
+
+console.log(task4.operationTimes2(countArray_1))
+console.log(task4.operationTimes2(countArray_2))
+console.log()
+
+try{
+    task5.main3(number)
+    console.log()
+}catch (err){
+    console.log(err.message);
+}
+
+console.log(task6.buatRentang(1, 5))
+console.log(task6.buatRentang(5, 10))
